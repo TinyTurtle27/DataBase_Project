d60967a5fa8b1487f2ef30554b43eb163dc277ac
bluekoyoyo <007060554@coyote.csusb.edu>
2024-05-03 04:22:58 -0700

    using sveltekit honestly kind of an arbitrary choice to use a web framework. you'll need npm, I recommend getting ubuntu from window subsystem for linux and using npm in that enviornment. currently using npm 10.5.1 and node v22.0.0

---
+++ front_end/schoolreg/schoolreg-app/src/routes/Select.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let givenoptions = []; 
+    export let widthvalue = '250';
+//    export let selectedValue = '';
+
+ //   let isOpen = false;
+
+    // Emits a 'select' event with the chosen value when an option is clicked
+//    const handleSelect = (value) => {
+//        selectedValue = value; 
+//        isOpen = false;
+//        // Dispatch the event for the parent component to listen to
+//        dispatch('select', value);
+//    } 
+</script>
+
+<select class="form-select" style="width: {widthvalue}px;">
+    {#each givenoptions as option}
+        <option value={option}>{option}</option>
+    {/each}
+</select>
+
+<style>
+    .form-select {
+        height: 20px;
+        /* Basic styling goes here */
+    }
+/* Basic styling goes here */ 
+</style>
