2414ba3eda0eebe9c1fff092bd0f7cffd086f844
TinyTurtle27 <161649097+TinyTurtle27@users.noreply.github.com>
2024-05-03 01:01:18 -0700

    update

--- Code/web.php
+++ Code/web.php
@@ -8,14 +8,72 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <div>Other included later</div>
+    
+    <div class="Registered Classes">
+        <table class="outer table" brder="True">
+            <caption>
+                <h3>Registered Classes</h3>
+                <h4>Term: Fall 2020</h4>
+            </caption>
 
-<?php
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Instructor</th>
+                    <th>Meeting Time</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>testing</td>
+                    <td>teacher</td>
+                    <td>
+                        <table border="True">
+                            <tr>
+                                <th>M</th>
+                                <th>T</th>
+                                <th>W</th>
+                                <th>TH</th>
+                                <th>F</th>
+                            </tr>
+                        </table>
+                    </td>
+                    <td>Registed</td>
+                </tr>
+                <tr>
+                    <td>tt</td>
+                    <td>d</td>
+                    <td>
+                        <table border="True">
+                            <tr>
+                                <th>M</th>
+                                <th>T</th>
+                                <th>W</th>
+                                <th>TH</th>
+                                <th>F</th>
+                            </tr>
+                        </table>
+                    </td>
+                    <td>w</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+
+
+
+
+<!-- <?php
         $query = $pdo->query('select * from student');
         $data = $query->fetchAll(PDO::FETCH_NUM);
         echo print_r($data);
-?>
+?> -->
     
 </body>
 </html>
