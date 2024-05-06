459c7e8c15c8f1c2fd8fe1b18c990cb013e3c6e6
bluekoyoyo <154391737+bluekoyoyo@users.noreply.github.com>
2024-05-05 20:50:48 -0700

    registration search frontend (#2)
    * Scraped courses for database, includes major, course title, units, and description, but could use some more processing before being used.
    
    * using sveltekit
    honestly kind of an arbitrary choice to use a web framework. you'll need
    npm, I recommend getting ubuntu from window subsystem for linux and
    using npm in that enviornment. currently using npm 10.5.1 and node v22.0.0

---
+++ front_end/schoolreg/schoolreg-app/src/routes/Checkbox.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let textlabel = '';
+    let isChecked = false;
+
+    function toggleCheckbox() {
+        isChecked = !isChecked;
+    }
+</script>
+
+<div class="namedcheckbox">
+    <input type="checkbox" bind:checked={isChecked} on:click={toggleCheckbox}>
+    <strong> <label for={textlabel}>{textlabel}</label> </strong>
+</div>
+
+<style>
+    .namedcheckbox {
+        display: flex;
+        align-items: center;
+        padding-right: 5px;
+    }
+</style>
